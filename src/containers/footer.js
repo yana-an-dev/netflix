805c12a806c51fc8db5b4bd686c665644c40b053
Yana <ayh916@gmail.com>
2021-08-30 10:15:16 +1000

    Styling the Footer Component

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -16,6 +16,14 @@ export function FooterContainer() {
                 </Footer.Column>
 
                 <Footer.Column>
+                    <Footer.Link href="#">Help Centre</Footer.Link>
+                    <Footer.Link href="#">Jobs</Footer.Link>
+                    <Footer.Link href="#">Terms of Use</Footer.Link>
+                    <Footer.Link href="#">Contact Us</Footer.Link>
+
+                </Footer.Column>
+
+                <Footer.Column>
                     <Footer.Link href="#">Account</Footer.Link>
                     <Footer.Link href="#">Redeem gift cards</Footer.Link>
                     <Footer.Link href="#">Privacy</Footer.Link>
