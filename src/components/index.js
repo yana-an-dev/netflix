a00f7f8815f0a806d0bb88920a5095b58beb8a05
Yana <ayh916@gmail.com>
2021-08-30 10:53:35 +1000

    Building the Accordion Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Jumbotron } from './jumbotron'
 export { default as Footer } from './footer'
+export { default as Accordion } from './accordion'
 //Component Library Tree Shaking (here used webpack due to scrimba)
 //(or Roll up)
