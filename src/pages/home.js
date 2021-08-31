e8c7953004e94d6d581735fafb3967b5fe18c71c
Yana <ayh916@gmail.com>
2021-08-31 11:15:14 +1000

    Styling the header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,19 @@ import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
 import { FaqsContainer } from '../containers/faqs'
 import { HeaderContainer } from '../containers/header'
+import { OptForm } from '../components'
+
+
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <p>Hello</p>
+                <OptForm>
+                    <OptForm.Text>Ready to watch Netflix? Enter your email to create or restart your membership.</OptForm.Text>
+                    <OptForm.Break />
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                </OptForm>
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
