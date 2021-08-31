b6e8ddc7e39bbb05de1e9b85edba62515507fad6
Yana <ayh916@gmail.com>
2021-08-31 14:21:50 +1000

    Buildling out the Feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
 import { FaqsContainer } from '../containers/faqs'
 import { HeaderContainer } from '../containers/header'
-import { OptForm } from '../components'
+import { OptForm, Feature } from '../components'
 
 
 export default function Home() {
@@ -11,10 +11,16 @@ export default function Home() {
         <>
             <HeaderContainer>
                 <OptForm>
-                    <OptForm.Text>Ready to watch Netflix? Enter your email to create or restart your membership.</OptForm.Text>
-                    <OptForm.Break />
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Try it now</OptForm.Button>
+                    <Feature>
+                        <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                        <Feature.Subtitle>Watch Anywhere. Cancel at anytime.</Feature.Subtitle>
+
+
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch Netflix? Enter your email to create or restart your membership.</OptForm.Text>
+                    </Feature>
                 </OptForm>
             </HeaderContainer>
             <JumbotronContainer />
